6ae3cd3c9ea3dd7862dcbad1559885a149b65063
Nataliia1113 <orlova.n1113@gmail.com>
2024-03-03 12:31:25 +0200

    Updade-HW9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,38 @@
-console.log("Form");
+const STORAGE_KEY = "feedback-form-state"
+
+const form = document.querySelector('.feedback-form')
+
+function getValueFromInput(form) {
+    const email = form.email.value.trim()
+    const message = form.message.value.trim()
+    return {
+        email,
+        message
+    }
+}
+
+form.addEventListener('input', event => {
+    const data = getValueFromInput(event.currentTarget)
+    const jsonData = JSON.stringify(data)
+    localStorage.setItem(STORAGE_KEY, jsonData)
+
+})
+
+const rawData = localStorage.getItem(STORAGE_KEY)
+
+if (rawData) {
+    const data = JSON.parse(rawData)
+    form.email.value = data.email
+    form.message.value = data.message
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    if (form.email.value === '' || form.message.value === '') {
+        alert('All form fields must be filled in')
+    } else {
+        console.log(getValueFromInput(event.currentTarget))
+        form.reset()
+        localStorage.clear()
+    }
+})
