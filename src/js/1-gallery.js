044eca532756d26843e31aea6c89ac1895b3d2ca
Nataliia1113 <orlova.n1113@gmail.com>
2024-03-03 12:39:35 +0200

    formatted-HW9

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,86 +1,83 @@
+import SimpleLightbox from 'simplelightbox';
 
-import SimpleLightbox from "simplelightbox";
-
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const images = [
-      {
-      id: 1,
-        preview:
-          'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__340.jpg',
-        original:
-          'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-        description: 'Hokkaido Flower',
-      },
-      {
-      id: 2,
-        preview:
-          'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-        original:
-          'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-        description: 'Container Haulage Freight',
-      },
-      {
-      id: 3,
-        preview:
-          'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-        original:
-          'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-        description: 'Aerial Beach View',
-      },
-      {
-      id: 4,
-        preview:
-          'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-        original:
-          'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-        description: 'Flower Blooms',
-      },
-      {
-      id: 5,
-        preview:
-          'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-        original:
-          'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-        description: 'Alpine Mountains',
-      },
-      {
-      id: 6,
-        preview:
-          'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-        original:
-          'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-        description: 'Mountain Lake Sailing',
-      },
-      {
-      id: 7,
-        preview:
-          'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-        original:
-          'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-        description: 'Alpine Spring Meadows',
-      },
-      {
-      id: 8,
-        preview:
-          'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-        original:
-          'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-        description: 'Nature Landscape',
-      },
-      {
-      id: 9,
-        preview:
-          'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-        original:
-          'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-        description: 'Lighthouse Coast Sea',
-      },
-    ];
-    const container = document.querySelector('.gallery');
- 
+  {
+    id: 1,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    id: 2,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    id: 3,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    id: 4,
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    id: 5,
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    id: 6,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    id: 7,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    id: 8,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    id: 9,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+const container = document.querySelector('.gallery');
 
-   
 function createGalleryItem({ id, preview, original, description }) {
   return `
     <li data-id="${id}" class="gallery-item">
@@ -117,13 +114,14 @@ function onGalleryClick(event) {
   const idClicked = card.dataset.id;
   console.log(idClicked);
 
-
   const largeImageUrl = target.dataset.source;
   const lightbox = basicLightbox.create(`
   <img src="${largeImageUrl}" alt="${target.alt}" width="800" height="600">
-`)
+`);
 
   lightbox.show();
 }
-const lightbox = new SimpleLightbox('.gallery a', {  captionsData: 'alt',
-captionDelay: 250 });
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,38 +1,37 @@
-const STORAGE_KEY = "feedback-form-state"
+const STORAGE_KEY = 'feedback-form-state';
 
-const form = document.querySelector('.feedback-form')
+const form = document.querySelector('.feedback-form');
 
 function getValueFromInput(form) {
-    const email = form.email.value.trim()
-    const message = form.message.value.trim()
-    return {
-        email,
-        message
-    }
+  const email = form.email.value.trim();
+  const message = form.message.value.trim();
+  return {
+    email,
+    message,
+  };
 }
 
 form.addEventListener('input', event => {
-    const data = getValueFromInput(event.currentTarget)
-    const jsonData = JSON.stringify(data)
-    localStorage.setItem(STORAGE_KEY, jsonData)
+  const data = getValueFromInput(event.currentTarget);
+  const jsonData = JSON.stringify(data);
+  localStorage.setItem(STORAGE_KEY, jsonData);
+});
 
-})
-
-const rawData = localStorage.getItem(STORAGE_KEY)
+const rawData = localStorage.getItem(STORAGE_KEY);
 
 if (rawData) {
-    const data = JSON.parse(rawData)
-    form.email.value = data.email
-    form.message.value = data.message
+  const data = JSON.parse(rawData);
+  form.email.value = data.email;
+  form.message.value = data.message;
 }
 
 form.addEventListener('submit', event => {
-    event.preventDefault()
-    if (form.email.value === '' || form.message.value === '') {
-        alert('All form fields must be filled in')
-    } else {
-        console.log(getValueFromInput(event.currentTarget))
-        form.reset()
-        localStorage.clear()
-    }
-})
+  event.preventDefault();
+  if (form.email.value === '' || form.message.value === '') {
+    alert('All form fields must be filled in');
+  } else {
+    console.log(getValueFromInput(event.currentTarget));
+    form.reset();
+    localStorage.clear();
+  }
+});
